7c6e6d6d12472feaa92243566bfb3f1be993934e
GaoKai <GaoKai>
2016-08-13 10:53:13 +0800

    update

--- Assets/Script/Core/Audio/AudioPlayer.cs
+++ Assets/Script/Core/Audio/AudioPlayer.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class AudioPlayer : MonoBehaviour {
-
+public class AudioPlayer : MonoBehaviour 
+{
+    public AudioCallBack m_completeCallBack;
+    public AudioSource m_player;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 	
 	}
+
+    public void Init(AudioClip l_audio,AudioCallBack l_callBack)
+    {
+
+    }
 }
