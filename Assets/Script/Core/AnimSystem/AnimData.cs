0d353044353c5f08ec8a33c226c75629cf53cc38
GaoKai <GaoKai>
2016-08-17 18:48:23 +0800

    animsystem

--- Assets/Script/Core/AnimSystem/AnimData.cs
+++ Assets/Script/Core/AnimSystem/AnimData.cs
@@ -8,11 +8,13 @@ public class AnimData
 {
     public AnimType animType;
     public InteType interpolationType;
+    public BezierType bezierType = BezierType.Bezier1;
 
     public GameObject animGameObejct;
 
     public float currentTime = 0;
     public float totalTime = 0;
+    public Vector3[] BezierContral = new Vector3[3]; //二阶取第一个用，三阶取前两个
 
     public bool isDone = false;
 
@@ -29,13 +31,20 @@ public class AnimData
             isDone = true;
         }
 
+
         switch (animType)
         {
             case AnimType.UGUI_alpha: UguiAlpha(); break;
             case AnimType.UGUI_anchoredPosition: UguiPosition(); break;
             case AnimType.Position: Position(); break;
             case AnimType.LocalPosition: LocalPosition(); break;
+            case AnimType.LocalScale: LocalScale(); break;
+            case AnimType.SizeDetal: SizeDelta(); break;
         }
+
+
+
+
     }
 
     public void executeCallBack()
@@ -59,6 +68,32 @@ public class AnimData
         {
             case InteType.Linear: return Mathf.Lerp(oldValue, aimValue, currentTime / totalTime);
             case InteType.InBack: return InBack(oldValue, aimValue, currentTime, totalTime);
+            case InteType.outBack: return OutBack(oldValue, aimValue, currentTime, totalTime);
+            case InteType.inOutBack: return InOutBack(oldValue, aimValue, currentTime, totalTime);
+            case InteType.outInBack: return OutInBack(oldValue, aimValue, currentTime, totalTime);
+            case InteType.inQuad: return InQuad(oldValue, aimValue, currentTime, totalTime);
+            case InteType.outQuad: return OutQuad(oldValue, aimValue, currentTime, totalTime);
+            case InteType.inoutQuad: return InoutQuad(oldValue, aimValue, currentTime, totalTime);
+            case InteType.inCubic: return InCubic(oldValue, aimValue, currentTime, totalTime);
+            case InteType.outCubic: return OutCubic(oldValue, aimValue, currentTime, totalTime);
+            case InteType.inoutCubic: return InoutCubic(oldValue, aimValue, currentTime, totalTime);
+            case InteType.inQuart: return InQuart(oldValue, aimValue, currentTime, totalTime);
+            case InteType.outQuart: return OutQuart(oldValue, aimValue, currentTime, totalTime);
+            case InteType.inOutQuart: return InOutQuart(oldValue, aimValue, currentTime, totalTime);
+            case InteType.outInQuart: return OutInQuart(oldValue, aimValue, currentTime, totalTime);
+            case InteType.inQuint: return InQuint(oldValue, aimValue, currentTime, totalTime);
+            case InteType.outQuint: return OutQuint(oldValue, aimValue, currentTime, totalTime);
+            case InteType.inOutQuint: return InOutQuint(oldValue, aimValue, currentTime, totalTime);
+            case InteType.outInQuint: return OutInQuint(oldValue, aimValue, currentTime, totalTime);
+            case InteType.inSine: return InSine(oldValue, aimValue, currentTime, totalTime);
+            case InteType.outSine: return OutSine(oldValue, aimValue, currentTime, totalTime);
+            case InteType.inOutSine: return InOutSine(oldValue, aimValue, currentTime, totalTime);
+            case InteType.outInSine: return OutInSine(oldValue, aimValue, currentTime, totalTime);
+            case InteType.inExpo: return InExpo(oldValue, aimValue, currentTime, totalTime);
+            case InteType.outExpo: return OutExpo(oldValue, aimValue, currentTime, totalTime);
+            case InteType.inOutExpo: return InOutExpo(oldValue, aimValue, currentTime, totalTime);
+            case InteType.outInExpo: return OutInExpo(oldValue, aimValue, currentTime, totalTime);
+
         }
 
         return 0;
@@ -66,13 +101,66 @@ public class AnimData
 
     Vector3 getInterpolationV3(Vector3 oldValue, Vector3 aimValue)
     {
+
         Vector3 result = new Vector3(
             getInterpolation(oldValue.x, aimValue.x),
             getInterpolation(oldValue.y, aimValue.y),
             getInterpolation(oldValue.z, aimValue.z)
-            );
+        );
+
+        if (bezierType != BezierType.Bezier1)
+        {
+            result = getBezierInterpolationV3(oldValue, aimValue);
+        }
 
         return result;
+
+
+
+
+    }
+
+    /// <summary>
+    /// 贝塞尔专用
+    /// </summary>
+    Vector3 getBezierInterpolationV3(Vector3 oldValue, Vector3 aimValue)
+    {
+
+        Vector3 result = new Vector3(
+            getInterpolation(oldValue.x, aimValue.x),
+            0,
+            0
+        );
+        float n_finishingRate = (result.x - oldValue.x) / (aimValue.x - oldValue.x);
+        n_finishingRate = Mathf.Clamp(n_finishingRate, -1, 2);
+
+        switch (bezierType)
+        {
+            case BezierType.Bezier2: return Bezier2(oldValue, aimValue, n_finishingRate, BezierContral);
+            case BezierType.Bezier3: return Bezier3(oldValue, aimValue, n_finishingRate, BezierContral);
+            default: return Vector3.zero;
+        }
+
+    }
+
+
+
+    /// <summary>
+    /// 二阶贝塞尔曲线函数
+    /// </summary>
+
+    Vector3 Bezier2(Vector3 startPos, Vector3 endPos, float n_time, Vector3[] v3_ControlPoint)
+    {
+        return (1 - n_time) * (1 - n_time) * startPos + 2 * (1 - n_time) * n_time * v3_ControlPoint[0] + n_time * n_time * endPos;
+    }
+
+    /// <summary>
+    /// 三阶贝塞尔曲线函数
+    /// </summary>
+
+    Vector3 Bezier3(Vector3 startPos, Vector3 endPos, float n_time, Vector3[] t_ControlPoint)
+    {
+        return (1 - n_time) * (1 - n_time) * (1 - n_time) * startPos + 3 * (1 - n_time) * (1 - n_time) * n_time * t_ControlPoint[0] + 3 * (1 - n_time) * n_time * n_time * t_ControlPoint[1] + n_time * n_time * n_time * endPos;
     }
 
 
@@ -194,6 +282,25 @@ public class AnimData
         transform.localPosition = getInterpolationV3(formPos, toPos);
     }
 
+    void LocalScale()
+    {
+        transform.localScale = getInterpolationV3(formPos, toPos);
+    }
+
+
+
+    RectTransform rectTransform;
+    void SizeDelta()
+    {
+        rectTransform = transform.GetComponent<RectTransform>();
+        if (rectTransform == null)
+        {
+            Debug.LogError(transform.name + "缺少RectTransform组件，不能进行sizeDelta变换！！");
+            return;
+        }
+        rectTransform.sizeDelta = getInterpolationV3(formPos, toPos);
+    }
+
     #endregion
 
     #region 插值算法
@@ -206,6 +313,331 @@ public class AnimData
 
         return c * t * t * ((s + 1) * t - s) + b;
     }
+
+    public float OutBack(float b, float to, float t, float d, float s = 1.70158f)
+    {
+        float c = to - b;
+
+        t = t / d - 1;
+
+        return c * (t * t * ((s + 1) * t + s) + 1) + b;
+
+    }
+
+    public float InOutBack(float b, float to, float t, float d, float s = 1.70158f)
+    {
+        float c = to - b;
+        s = s * 1.525f;
+        t = t / d * 2;
+        if (t < 1)
+            return c / 2 * (t * t * ((s + 1) * t - s)) + b;
+        else
+        {
+            t = t - 2;
+            return c / 2 * (t * t * ((s + 1) * t + s) + 2) + b;
+        }
+
+    }
+
+    public float OutInBack(float b, float to, float t, float d, float s = 1.70158f)
+    {
+        float c = to - b;
+        if (t < d / 2)
+        {
+            t *= 2;
+            c *= 0.5f;
+            t = t / d * 2;
+
+            t = t / d - 1;
+            return c * (t * t * ((s + 1) * t + s) + 1) + b;
+
+        }
+
+        else
+        {
+            t = t * 2 - d;
+            b += c * 0.5f;
+            c *= 0.5f;
+
+
+            if (t < 1)
+                return c / 2 * (t * t * ((s + 1) * t - s)) + b;
+            else
+            {
+                t = t - 2;
+                return c / 2 * (t * t * ((s + 1) * t + s) + 2) + b;
+            }
+        }
+
+
+    }
+
+    public float InQuad(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        t = t / d;
+        return (float)(c * Math.Pow(t, 2) + b);
+    }
+
+    public float OutQuad(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        t = t / d;
+        return (float)(-c * t * (t - 2) + b);
+    }
+
+    public float InoutQuad(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        t = t / d * 2;
+        if (t < 1)
+            return (float)(c / 2 * Math.Pow(t, 2) + b);
+        else
+            return -c / 2 * ((t - 1) * (t - 3) - 1) + b;
+
+    }
+    public float InCubic(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        t = t / d;
+        return (float)(c * Math.Pow(t, 3) + b);
+
+    }
+    public float OutCubic(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        t = t / d - 1;
+        return (float)(c * (Math.Pow(t, 3) + 1) + b);
+
+    }
+    public float InoutCubic(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        t = t / d * 2;
+        if (t < 1)
+            return c / 2 * t * t * t + b;
+        else
+        {
+            t = t - 2;
+            return c / 2 * (t * t * t + 2) + b;
+        }
+    }
+    public float InQuart(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        t = t / d;
+        return (float)(c * Math.Pow(t, 4) + b);
+
+    }
+    public float OutQuart(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        t = t / d - 1;
+        return (float)(-c * (Math.Pow(t, 4) - 1) + b);
+
+    }
+
+    public float InOutQuart(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        t = t / d * 2;
+        if (t < 1)
+            return (float)(c / 2 * Math.Pow(t, 4) + b);
+        else
+        {
+            t = t - 2;
+            return (float)(-c / 2 * (Math.Pow(t, 4) - 2) + b);
+        }
+
+    }
+    public float OutInQuart(float b, float to, float t, float d)
+    {
+        if (t < d / 2)
+        {
+            float c = to - b;
+            t *= 2;
+            c *= 0.5f;
+            t = t / d - 1;
+
+            return (float)(-c * (Math.Pow(t, 4) - 1) + b);
+        }
+        else
+        {
+            float c = to - b;
+            t = t * 2 - d;
+            b = b + c * 0.5f;
+            c *= 0.5f;
+            t = t / d;
+
+
+            return (float)(c * Math.Pow(t, 4) + b);
+
+        }
+    }
+
+    public float InQuint(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        t = t / d;
+        return (float)(c * Math.Pow(t, 5) + b);
+
+    }
+
+    public float OutQuint(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        t = t / d - 1;
+        return (float)(c * (Math.Pow(t, 5) + 1) + b);
+    }
+
+    public float InOutQuint(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        t = t / d * 2;
+        if (t < 1)
+            return (float)(c / 2 * Math.Pow(t, 5) + b);
+        else
+        {
+            t = t - 2;
+            return (float)(c / 2 * (Math.Pow(t, 5) + 2) + b);
+
+        }
+
+    }
+
+    public float OutInQuint(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        if (t < d / 2)
+        {
+            t *= 2;
+            c *= 0.5f;
+            t = t / d - 1;
+            return (float)(c * (Math.Pow(t, 5) + 1) + b);
+        }
+        else
+        {
+            t = t * 2 - d;
+            b = b + c * 0.5f;
+            c *= 0.5f;
+
+            t = t / d;
+            return (float)(c * Math.Pow(t, 5) + b);
+        }
+    }
+
+    public float InSine(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        return (float)(-c * Math.Cos(t / d * (Math.PI / 2)) + c + b);
+
+    }
+
+    public float OutSine(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        return (float)(c * Math.Sin(t / d * (Math.PI / 2)) + b);
+    }
+
+    public float InOutSine(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        return (float)(-c / 2 * (Math.Cos(Math.PI * t / d) - 1) + b);
+
+    }
+    public float OutInSine(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        if (t < d / 2)
+        {
+            t *= 2;
+            c *= 0.5f;
+            return (float)(c * Math.Sin(t / d * (Math.PI / 2)) + b);
+        }
+        else
+        {
+            t = t * 2 - d;
+            b += c * 0.5f;
+            c *= 0.5f;
+            return (float)(-c * Math.Cos(t / d * (Math.PI / 2)) + c + b);
+
+        }
+    }
+    public float InExpo(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        if (t == 0)
+            return b;
+        else
+            return (float)(c * Math.Pow(2, 10 * (t / d - 1)) + b - c * 0.001f);
+    }
+    public float OutExpo(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        if (t == d)
+            return b + c;
+        else
+            return (float)(c * 1.001 * (-Math.Pow(2, -10 * t / d) + 1) + b);
+
+    }
+    public float InOutExpo(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        if (t == 0)
+            return b;
+        if (t == d)
+            return (b + c);
+
+        t = t / d * 2;
+
+        if (t < 1)
+            return (float)(c / 2 * Math.Pow(2, 10 * (t - 1)) + b - c * 0.0005f);
+        else
+        {
+            t = t - 1;
+            return (float)(c / 2 * 1.0005 * (-Math.Pow(2, -10 * t) + 2) + b);
+
+        }
+
+
+    }
+
+    public float OutInExpo(float b, float to, float t, float d)
+    {
+        float c = to - b;
+        if (t < d / 2)
+        {
+            t *= 2;
+            c *= 0.5f;
+            if (t == d)
+                return b + c;
+            else
+                return (float)(c * 1.001 * (-Math.Pow(2, -10 * t / d) + 1) + b);
+        }
+        else
+        {
+            t = t * 2 - d;
+            b += c * 0.5f;
+            c *= 0.5f;
+            if (t == 0)
+                return b;
+            else
+                return (float)(c * Math.Pow(2, 10 * (t / d - 1)) + b - c * 0.001f);
+
+        }
+    }
+
+
+
+
+    //outInExpo,
+    //inBack,
+    //outBack,
+    //inOutBack,
+    //outInBack,
+
+
+
 
     #endregion
 }
--- Assets/Script/Core/AnimSystem/AnimSystem.cs
+++ Assets/Script/Core/AnimSystem/AnimSystem.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System;
 
 public class AnimSystem : MonoBehaviour
 {
@@ -45,6 +46,8 @@ public class AnimSystem : MonoBehaviour
         getInstance().animList.Add(DataTmp);
     }
 
+
+
     public static void Move(GameObject animObject, Vector3 from, Vector3 to, float time, InteType interpolationType = InteType.Default, bool isLocal = true, AnimCallBack callBack = null, params object[] parameter)
     {
         AnimData DataTmp = getAnimData(animObject, time, interpolationType, callBack, parameter);
@@ -57,6 +60,103 @@ public class AnimSystem : MonoBehaviour
         {
             DataTmp.animType = AnimType.Position;
         }
+
+
+        DataTmp.formPos = from;
+        DataTmp.toPos = to;
+        DataTmp.TransfromInit();
+
+        getInstance().animList.Add(DataTmp);
+    }
+
+    #region 贝塞尔移动方法群
+    public static void BezierMove(GameObject animObject, Vector3 from, Vector3 to, float time, InteType interpolationType = InteType.Default, bool isLocal = true, BezierType bezierMoveType = BezierType.Bezier2, AnimCallBack callBack = null, Vector3[] t_Bezier_contral = null, params object[] parameter)
+    {
+        AnimData DataTmp = getAnimData(animObject, time, interpolationType, callBack, parameter);
+
+        if (isLocal)
+        {
+            DataTmp.animType = AnimType.LocalPosition;
+        }
+        else
+        {
+            DataTmp.animType = AnimType.Position;
+        }
+        DataTmp.bezierType = bezierMoveType;
+        DataTmp.BezierContral = t_Bezier_contral;
+        DataTmp.formPos = from;
+        DataTmp.toPos = to;
+        DataTmp.TransfromInit();
+
+        getInstance().animList.Add(DataTmp);
+
+    }
+
+    public static void BezierMove(GameObject animObject, Vector3 from, Vector3 to, float time, InteType interpolationType = InteType.Default, bool isLocal = true, BezierType bezierMoveType = BezierType.Bezier2, AnimCallBack callBack = null, float[] t_Bezier_contralRadius = null, params object[] parameter)
+    {
+
+        //三阶贝塞尔，两个控制点分别以起点和终点为中心进行随机
+        Vector3[] t_Bezier_contral = new Vector3[3];
+        t_Bezier_contral[0] = UnityEngine.Random.insideUnitSphere * t_Bezier_contralRadius[0] + from;
+        t_Bezier_contral[1] = UnityEngine.Random.insideUnitSphere * t_Bezier_contralRadius[1] + to;
+
+        //二阶贝塞尔，控制点以起点与终点的中间为随机中心
+        if (bezierMoveType == BezierType.Bezier2)
+        {
+            t_Bezier_contral[0] = UnityEngine.Random.insideUnitSphere * t_Bezier_contralRadius[0] + (from + to) * 0.5f;
+        }
+
+        //如果在平面内（z轴相同），控制点也要在该平面内
+        if (from.z == to.z)
+        {
+            t_Bezier_contral[0].z = from.z;
+            t_Bezier_contral[1].z = from.z;
+        }
+
+
+        BezierMove(animObject, from, to, time, interpolationType, isLocal, bezierMoveType, callBack, t_Bezier_contral, parameter);
+
+    }
+    public static void BezierMove(GameObject animObject, Vector3 to, float time, InteType interpolationType = InteType.Default, bool isLocal = true, BezierType bezierMoveType = BezierType.Bezier2, AnimCallBack callBack = null, float[] t_Bezier_contralRadius = null, params object[] parameter)
+    {
+        Vector3 from;
+        if (isLocal)
+        {
+            from = animObject.transform.localPosition;
+        }
+        else
+        {
+            from = animObject.transform.position;
+        }
+
+        BezierMove(animObject, from, to, time, interpolationType, isLocal, bezierMoveType, callBack, t_Bezier_contralRadius, parameter);
+    }
+
+
+    #endregion
+
+    public static void Scale(GameObject animObject, Vector3 from, Vector3 to, float time, InteType interpolationType = InteType.Default, bool isLocal = true, AnimCallBack callBack = null, params object[] parameter)
+    {
+        AnimData DataTmp = getAnimData(animObject, time, interpolationType, callBack, parameter);
+
+
+        DataTmp.animType = AnimType.LocalScale;
+
+
+        DataTmp.formPos = from;
+        DataTmp.toPos = to;
+        DataTmp.TransfromInit();
+
+        getInstance().animList.Add(DataTmp);
+    }
+
+    public static void SizeDelta(GameObject animObject, Vector3 from, Vector3 to, float time, InteType interpolationType = InteType.Default, bool isLocal = true, AnimCallBack callBack = null, params object[] parameter)
+    {
+        AnimData DataTmp = getAnimData(animObject, time, interpolationType, callBack, parameter);
+
+
+        DataTmp.animType = AnimType.SizeDetal;
+
 
         DataTmp.formPos = from;
         DataTmp.toPos = to;
@@ -87,6 +187,66 @@ public class AnimSystem : MonoBehaviour
         return DataTmp;
     }
 
+    public static void ValueTo(GameObject l_gameObject, params object[] l_params)
+    {
+        AnimData DataTmp = new AnimData();
+
+        for (int i = 0; i < l_params.Length; i += 2)
+        {
+            AnimParamType l_ParamType = (AnimParamType)l_params[i];
+            switch (l_ParamType)
+            {
+                case AnimParamType.FromV3: DataTmp.formPos = (Vector3)l_params[i + 1]; break;
+                case AnimParamType.FromV2: DataTmp.formPos = (Vector2)l_params[i + 1]; break;
+                case AnimParamType.FromColor: DataTmp.formPos = (Color)l_params[i + 1]; break;
+                case AnimParamType.FromFloat: DataTmp.formPos = (float)l_params[i + 1]; break;
+
+                case AnimParamType.ToV3: DataTmp.formPos = (Vector3)l_params[i + 1]; break;
+                case AnimParamType.ToV2: DataTmp.formPos = (Vector3)l_params[i + 1]; break;
+                case AnimParamType.ToFloat: DataTmp.formPos = (Vector3)l_params[i + 1]; break;
+                case AnimParamType.ToColor: DataTmp.formPos = (Vector3)l_params[i + 1]; break;
+
+                case AnimParamType.AnimType: DataTmp.formPos = (Vector3)l_params[i + 1]; break;
+                case AnimParamType.Time: DataTmp.formPos = (Vector3)l_params[i + 1]; break;
+                case AnimParamType.InteType: DataTmp.formPos = (Vector3)l_params[i + 1]; break;
+                case AnimParamType.RoadType: DataTmp.formPos = (Vector3)l_params[i + 1]; break;
+
+
+
+
+
+
+
+            }
+        }
+    }
+
+    public enum AnimParamType
+    {
+        FromV3,
+        FromV2,
+        FromFloat,
+        FromColor,
+
+        ToV3,
+        ToV2,
+        ToFloat,
+        ToColor,
+
+        AnimType,
+        Time,
+        InteType,
+        RoadType,
+
+        RepeatType,
+
+        IsChild,
+        IsLocal,
+
+        CallBack,
+        CallBackParams
+    }
+
     #endregion
 
     #region 实例部分
@@ -98,6 +258,7 @@ public class AnimSystem : MonoBehaviour
     {
         for (int i = 0; i < animList.Count; i++)
         {
+
             //执行Update
             animList[i].executeUpdate();
 
@@ -123,7 +284,10 @@ public enum AnimType
     LocalPosition,
     Position,
     UGUI_alpha,
-    UGUI_anchoredPosition
+    UGUI_anchoredPosition,
+    LocalScale,
+    SizeDetal,
+
 }
 
 //插值算法类型
@@ -132,4 +296,38 @@ public enum InteType
     Default,
     Linear,
     InBack,
+    outBack,
+    inOutBack,
+    outInBack,
+    inQuad,
+    outQuad,
+    inoutQuad,
+    inCubic,
+    outCubic,
+    inoutCubic,
+    outInCubic,
+    inQuart,
+    outQuart,
+    inOutQuart,
+    outInQuart,
+    inQuint,
+    outQuint,
+    inOutQuint,
+    outInQuint,
+    inSine,
+    outSine,
+    inOutSine,
+    outInSine,
+    inExpo,
+    outExpo,
+    inOutExpo,
+    outInExpo,
+}
+
+public enum BezierType
+{
+    Bezier1,
+    Bezier2,
+    Bezier3,
+
 }
