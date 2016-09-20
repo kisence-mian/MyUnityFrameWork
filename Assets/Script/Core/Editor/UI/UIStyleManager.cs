275fccc50946d333f244dd21ce984cb6445b5ab4
GaoKai <kxjhaha@vip.qq.com>
2016-09-20 17:58:19 +0800

    修复BUG

--- Assets/Script/Core/Editor/UI/UIStyleManager.cs
+++ Assets/Script/Core/Editor/UI/UIStyleManager.cs
@@ -61,8 +61,6 @@ public class UIStyleManager
                     }
                 }
 
-
-
                 EditorGUILayout.Space();
                 EditorGUILayout.EndHorizontal();
             }
