2a547951c4f5621765e759e503b07a767e98fd62
高凯 <kxjhaha@gmail.com>
2016-10-31 09:11:37 +0800

    提交

---
+++ Assets/Script/Core/Record/RecordManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecordManager 
+{
+    public const string c_directoryName = "Record";
+    public const string c_expandName = "json";
+
+
+}
---
+++ Assets/Script/Core/Record/RecordTable.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class RecordTable : Dictionary<string, SingleField> 
+{
+    int @string = 1;
+
+    public SingleField GetRecord(string key)
+    {
+        if(this.ContainsKey(key))
+        {
+            return this[key];
+        }
+        else
+        {
+            return null;
+        }
+
+        WeakReference wr = new WeakReference(new GameObject());
+        GameObject go = wr.Target as GameObject;
+
+        if(wr.IsAlive)
+        {
+            go = wr.Target as GameObject;
+        }
+        else
+        {
+
+        }
+
+
+        
+
+
+
+
+    }
+}
