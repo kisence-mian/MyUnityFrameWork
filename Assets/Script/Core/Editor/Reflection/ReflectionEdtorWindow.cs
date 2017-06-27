98ad8a7339afe7413514b19c2d6ee92fbe1fda69
GaoKai <kxjhaha@vip.qq.com>
2017-06-27 19:02:45 +0800

    SDKManager,和两个界面

--- Assets/Script/Core/Editor/Reflection/ReflectionEdtorWindow.cs
+++ Assets/Script/Core/Editor/Reflection/ReflectionEdtorWindow.cs
@@ -22,8 +22,6 @@ public class ReflectionEdtorWindow : EditorWindow
 
         MainGUI();
 
-
-
         //命名空间
 
         //类
@@ -39,6 +37,8 @@ public class ReflectionEdtorWindow : EditorWindow
 
     WindowStatus m_status = WindowStatus.AppDomain;
     Assembly m_currentAssembly;
+    Type m_currentClass;
+    MethodInfo m_method;
 
     void MainGUI()
     {
@@ -53,7 +53,13 @@ public class ReflectionEdtorWindow : EditorWindow
                 break;
 
             case WindowStatus.Assembly:
-                NameSpaceGUI();
+                //类
+                AllClassGUI();
+                break;
+
+            case WindowStatus.Class:
+                //类
+                AllMethodGUI();
                 break;
 
             default:break;
@@ -68,12 +74,14 @@ public class ReflectionEdtorWindow : EditorWindow
         {
             content += "应用程序视图：";
             content += AppDomain.CurrentDomain.FriendlyName;
-
         }
         else if(m_status == WindowStatus.Assembly)
         {
             content += "程序集视图：";
-
+        }
+        else if (m_status == WindowStatus.Class)
+        {
+            content += "类视图：" + m_currentClass.Name;
         }
 
         EditorGUILayout.LabelField(content);
@@ -111,29 +119,41 @@ public class ReflectionEdtorWindow : EditorWindow
 
     #region 命名空间
 
-    Module[] m_module;
-    Type[] m_types;
-    Vector2 m_nameSpace = Vector2.zero;
+    //Module[] m_module;
+
 
     void NameSpaceGUI()
     {
-        if(m_currentAssembly != null)
+    }
+
+    #endregion
+
+    #region 类
+
+    Type[] m_types;
+    Vector2 m_classSpace = Vector2.zero;
+
+    void AllClassGUI()
+    {
+        if (m_currentAssembly != null)
         {
-            if(m_module == null)
+            if(m_types == null )
             {
-                m_module = m_currentAssembly.GetModules(true);
+                m_types = m_currentAssembly.GetTypes();
             }
 
-            m_nameSpace = GUILayout.BeginScrollView(m_nameSpace);
+            m_classSpace = GUILayout.BeginScrollView(m_classSpace);
 
-            for (int i = 0; i < m_module.Length; i++)
+            for (int i = 0; i < m_types.Length; i++)
             {
-                EditorGUILayout.LabelField(m_module[i].Name);
-            }
-
-            for (int i = 0; i < m_currentAssembly.GetManifestResourceNames().Length; i++)
-            {
-                EditorGUILayout.LabelField(m_currentAssembly.GetManifestResourceNames()[i]);
+                if(m_types[i].IsClass)
+                {
+                    if (GUILayout.Button(m_types[i].Name))
+                    {
+                        m_status = WindowStatus.Class;
+                        m_currentClass = m_types[i];
+                    }
+                }
             }
 
             GUILayout.EndScrollView();
@@ -143,21 +163,47 @@ public class ReflectionEdtorWindow : EditorWindow
             EditorGUILayout.LabelField("没有选中程序集");
         }
 
-        if(GUILayout.Button("返回上一层"))
+        if (GUILayout.Button("返回上一层"))
         {
             m_status = WindowStatus.AppDomain;
             m_currentAssembly = null;
-            m_module = null;
+            m_types = null;
         }
     }
-
-    #endregion
-
-    #region 类
-
     #endregion
 
     #region 方法
+
+    void AllMethodGUI()
+    {
+        if (m_currentClass != null)
+        {
+            m_classSpace = GUILayout.BeginScrollView(m_classSpace);
+
+            MethodInfo[] typesTmp = m_currentClass.GetMethods(BindingFlags.Public| BindingFlags.NonPublic| BindingFlags.Static| BindingFlags.Instance);
+
+            for (int i = 0; i < typesTmp.Length; i++)
+            {
+                if (GUILayout.Button(typesTmp[i].Name))
+                {
+                    m_status = WindowStatus.Method;
+                    m_method = typesTmp[i];
+                }
+            }
+
+            GUILayout.EndScrollView();
+        }
+        else
+        {
+            EditorGUILayout.LabelField("没有选中程序集");
+        }
+
+        if (GUILayout.Button("返回上一层"))
+        {
+            m_status = WindowStatus.Assembly;
+            m_currentClass = null;
+        }
+    }
 
     #endregion
 
@@ -169,7 +215,7 @@ public class ReflectionEdtorWindow : EditorWindow
     {
         AppDomain,
         Assembly,
-        NameSpace,
+        //NameSpace,
         Class,
         Method,
     }
