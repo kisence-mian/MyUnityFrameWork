aeb32b9484cb1d86273477e4f150649efb89c0c3
GaoKai <kxjhaha@vip.qq.com>
2017-11-21 18:30:59 +0800

    RecordEntity

---
+++ Assets/Script/UI/heiheiheiWindow/heiheiheiWindow.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public class heiheiheiWindow : UIWindowBase 
+{
+
+    //UI的初始化请放在这里
+    public override void OnInit()
+    {
+
+    }
+
+    //请在这里写UI的更新逻辑，当该UI监听的事件触发时，该函数会被调用
+    public override void OnRefresh()
+    {
+
+    }
+
+    //UI的进入动画
+    public override IEnumerator EnterAnim(UIAnimCallBack l_animComplete, UICallBack l_callBack, params object[] objs)
+    {
+        AnimSystem.UguiAlpha(gameObject, null, 1, callBack:(object[] obj)=>
+        {
+            StartCoroutine(base.EnterAnim(l_animComplete, l_callBack, objs));
+        });
+
+        yield return new WaitForEndOfFrame();
+    }
+
+    //UI的退出动画
+    public override IEnumerator ExitAnim(UIAnimCallBack l_animComplete, UICallBack l_callBack, params object[] objs)
+    {
+        AnimSystem.UguiAlpha(gameObject , null, 0, callBack:(object[] obj) =>
+        {
+            StartCoroutine(base.ExitAnim(l_animComplete, l_callBack, objs));
+        });
+
+        yield return new WaitForEndOfFrame();
+    }
+}
