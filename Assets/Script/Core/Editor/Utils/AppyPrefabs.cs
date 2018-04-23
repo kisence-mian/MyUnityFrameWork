283e0cba516255f05d19abdfecfd937dbb78b835
isagic <isagic@gmail.com>
2018-04-23 15:42:50 +0800

    ：）用了个2018.2的API，兼容一下
    2018.2的API，兼容一下

--- Assets/Script/Core/Editor/Utils/AppyPrefabs.cs
+++ Assets/Script/Core/Editor/Utils/AppyPrefabs.cs
@@ -1,4 +1,4 @@
-﻿using UnityEditor;
+using UnityEditor;
 using UnityEngine;
 
 public class AppyPrefabs : Editor
@@ -17,13 +17,17 @@ public class AppyPrefabs : Editor
             PrefabType pt = PrefabUtility.GetPrefabType(currobj);
             if (pt == PrefabType.None) continue;
 
+#if UNITY_2018_2_OR_NEWER
             Object tg = PrefabUtility.GetCorrespondingObjectFromSource(currobj);
+#else
+            Object tg = PrefabUtility.GetPrefabParent(currobj);
+#endif       
             if (tg == null) continue;
-
-            //Debug.Log(currobj.name +"===="+ tg.name);
-
-            PrefabUtility.ReplacePrefab(currobj, tg,ReplacePrefabOptions.ConnectToPrefab);
+            //Debug.Log(currobj.name + "====" + tg.name);
+            PrefabUtility.ReplacePrefab(currobj, tg, ReplacePrefabOptions.ConnectToPrefab);
             PrefabUtility.ResetToPrefabState(currobj);
+            //重设后有子节点，去掉，在导出后不会影响已经加过子节的Hierarchy Object
+            //PrefabUtility.RevertPrefabInstance(currobj);
         }
         AssetDatabase.Refresh();
     }
@@ -39,10 +43,11 @@ public class AppyPrefabs : Editor
             GameObject currobj = PrefabUtility.FindRootGameObjectWithSameParentPrefab(item);
             if (currobj == null) continue;
 
-            //Debug.Log(currobj.name);
+            Debug.Log(currobj.name);
 
-			PrefabUtility.ReconnectToLastPrefab(currobj);//must reconnect
+            PrefabUtility.ReconnectToLastPrefab(currobj);//must reconnect
             PrefabUtility.ResetToPrefabState(currobj);
+            PrefabUtility.RevertPrefabInstance(currobj);//会全部重设
         }
         AssetDatabase.Refresh();
     }
